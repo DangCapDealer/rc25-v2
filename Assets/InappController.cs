fc331c95c73b893c22033b64d3c4bb59f2b7a1d7
Dan choi den tu Hung Yen <love.leosg@gmail.com>
2025-02-17 15:02:26 +0700

    update fix bug

--- Assets/InappController.cs
+++ Assets/InappController.cs
@@ -148,6 +148,7 @@ public class InappController : MonoBehaviour, IDetailedStoreListener
         if (AntiSpamClick() == true) return;
         var index = GetProductIndexById(productId);
         Debug.Log($"[InappPurchase] Buy IAP: {productId} | index: {index}");
+        AdManager.Instance.OpenAdSpaceTimeCounter = 0;
 #if UNITY_EDITOR
         OnPurchaseComplete = (complete) =>
         {
