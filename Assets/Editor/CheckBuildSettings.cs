af8e5b152c40f66437230d86b799eb12377a2c39
Dan choi den tu Hung Yen <206834414+danchoiVietNamne@users.noreply.github.com>
2025-08-04 16:06:12 +0700

    update

---
+++ Assets/Editor/CheckBuildSettings.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+class CheckBuildSettings : IPreprocessBuildWithReport
+{
+    public int callbackOrder { get { return 0; } }
+
+    public void OnPreprocessBuild(BuildReport report)
+    {
+        Debug.Log("Development Build is currently set to: " + EditorUserBuildSettings.development);
+        if (EditorUserBuildSettings.development)
+        {
+            Debug.LogError("CẢNH BÁO: Development Build đang được bật! Có thể do một plugin hoặc script khác đã thay đổi giá trị này.");
+        }
+    }
+}
